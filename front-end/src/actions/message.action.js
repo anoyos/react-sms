0411bdb8d3a16f1fbbe29f96e70bb7a7eaf7eebe
WebStar713 <webdeveloper713@gmail.com>
2020-02-11 05:00:28 +0100

    add message and voicemails action

--- front-end/src/actions/message.action.js
+++ front-end/src/actions/message.action.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import CONFIG from '../constants/config'
 import * as CONSTS from '../constants/const'
-
+import { getUserData } from './auth.action'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 export const sendMessage = (
   toNumber,
@@ -80,28 +80,52 @@ export const sendMessage = (
       })
   }
 }
-export const getMessage = (toNumber, fromNumber, fromNums) => {
+export const getMessage = (toNumber, fromNumber) => {
+  return async dispatch => {
+    if (toNumber && fromNumber) {
+      const msgData = {
+        fromNumber: fromNumber,
+        toNumber: toNumber,
+      }
+      await axios
+        .post(`${CONFIG.serverURL}/getmessages`, {
+          msgData,
+        })
+        .then(res => {
+          dispatch({ type: CONSTS.GET_ALL_MESSAGES, payload: res.data })
+          dispatch({
+            type: CONSTS.SMS_NOTIFICATION,
+            payload: { state: false, fromNumber: '' },
+          })
+        })
+    }
+  }
+}
+export const getPrintMessage = (toNumber, fromNumber, startDate, endDate) => {
   return async dispatch => {
     const msgData = {
       fromNumber: fromNumber,
       toNumber: toNumber,
-      fromNums: fromNums,
+      startDate: startDate,
+      endDate: endDate,
     }
+
     await axios
-      .post(`${CONFIG.serverURL}/getmessages`, {
+      .post(`${CONFIG.serverURL}/getprintmessages`, {
         msgData,
       })
       .then(res => {
         dispatch({ type: CONSTS.GET_ALL_MESSAGES, payload: res.data })
-        dispatch(getAllNumbers(fromNumber))
       })
   }
 }
-export const getAllNumbers = userNumber => {
+
+export const getAllNumbers = (userNumber, email) => {
   return async dispatch => {
     await axios
       .post(`${CONFIG.serverURL}/getnumbers`, {
         userNumber: userNumber,
+        email: email,
       })
       .then(res => {
         dispatch({ type: CONSTS.GET_ALL_USERS, payload: res.data })
@@ -116,7 +140,19 @@ export const saveUserNumber = (userNumber, userEmail) => {
         email: userEmail,
       })
       .then(res => {
-        dispatch({ type: CONSTS.GET_ALL_USERS, payload: res.data })
+        if (res.status === 200) dispatch(getUserData())
+      })
+  }
+}
+export const saveStyleMode = (mode, userEmail) => {
+  return async dispatch => {
+    await axios
+      .post(`${CONFIG.serverURL}/savestylemode`, {
+        styleMode: mode,
+        email: userEmail,
+      })
+      .then(res => {
+        if (res.status === 200) dispatch(getUserData())
       })
   }
 }
@@ -133,12 +169,84 @@ export const setMemberNum = data => {
     dispatch({ type: CONSTS.SET_MEM_NUMBER, payload: data })
   }
 }
-export const newMssage = data => {
+export const newMssage = (data, email) => {
   return (dispatch, getState) => {
     const { message } = getState()
     const { mem_number, numbers } = message
-    if (data.fromNumber === mem_number) {
-      dispatch(getMessage(data.fromNumber, data.toNumber, numbers.numberList))
+    const { savedNumber } = numbers
+    if (savedNumber && data.toNumber === savedNumber) {
+      dispatch(getAllNumbers(data.toNumber, email))
+      const notifiations = { state: true, fromNumber: data.toNumber }
+      dispatch({ type: CONSTS.SMS_NOTIFICATION, payload: notifiations })
+      dispatch(getUserData())
+      setTimeout(() => {
+        if (data.fromNumber === mem_number) {
+          dispatch(getMessage(data.fromNumber, data.toNumber))
+          dispatch(getUserData())
+        }
+      }, 2000)
+    }
+  }
+}
+
+export const deleteConversation = (toNumber, fromNumber, email) => {
+  return async dispatch => {
+    if (toNumber && fromNumber) {
+      const msgData = {
+        fromNumber: fromNumber,
+        toNumber: toNumber,
+      }
+      await axios
+        .post(`${CONFIG.serverURL}/deleteconversation`, {
+          msgData,
+        })
+        .then(res => {
+          if (res.status === 200) {
+            dispatch(getAllNumbers(fromNumber, email))
+            dispatch(getMessage(fromNumber, toNumber))
+          }
+        })
+    }
+  }
+}
+export const addFavoriteMessage = (fromNumber, toNumber, email) => {
+  return async dispatch => {
+    if (toNumber && fromNumber) {
+      const msgData = {
+        fromNumber: fromNumber,
+        toNumber: toNumber,
+        email: email,
+      }
+      await axios
+        .post(`${CONFIG.serverURL}/addfavorite`, {
+          msgData,
+        })
+        .then(res => {
+          if (res.status === 200) {
+            dispatch(getAllNumbers(fromNumber, email))
+          }
+        })
+    }
+  }
+}
+
+export const deleteFavoriteMessage = (fromNumber, toNumber, email) => {
+  return async dispatch => {
+    if (toNumber && fromNumber) {
+      const msgData = {
+        fromNumber: fromNumber,
+        toNumber: toNumber,
+        email: email,
+      }
+      await axios
+        .post(`${CONFIG.serverURL}/deletefavorite`, {
+          msgData,
+        })
+        .then(res => {
+          if (res.status === 200) {
+            dispatch(getAllNumbers(fromNumber, email))
+          }
+        })
     }
   }
 }
